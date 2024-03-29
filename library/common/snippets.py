9108bf757ce1e7270bde8a69d0977bd920d82c9b
Stavros kois <s.kois@outlook.com>
2024-03-29 20:44:56 +0200

    add some goodies

--- library/common/snippets.py
+++ library/common/snippets.py
@@ -1,11 +1,29 @@
 import yaml
 
-def healthcheck_timeouts(interval=10, timeout=10, retries=5, start_period=30):
+YAML_OPTS = {
+  'default_flow_style': False,
+  'sort_keys': False,
+  'indent': 2,
+}
+
+def healthcheck(test="", interval=10, timeout=10, retries=5, start_period=30):
+  if not test:
+    raise ValueError("Healtcheck: [test] must be set")
+
   timeouts = {
-    "interval": interval,
-    "timeout": timeout,
-    "retries": retries,
-    "start_period": start_period
+    "healthcheck": {
+      "test": test,
+      "interval": interval,
+      "timeout": timeout,
+      "retries": retries,
+      "start_period": start_period
+    }
   }
 
-  return yaml.dump(timeouts)
+  return yaml.dump(timeouts, **YAML_OPTS)
+
+def curl_test(url):
+  return f"curl --silent --fail {url}"
+
+def pg_test(host, user, db, port=5432):
+  return f"pg_isready -h {host} -p {port} -d {db} -U {user}"
