1ee04734a3361387f2dc23681055ab7551534e2b
Stavros kois <s.kois@outlook.com>
2024-03-29 20:18:41 +0200

    use yaml

--- library/common/snippets.py
+++ library/common/snippets.py
@@ -1,8 +1,11 @@
-import textwrap
+import yaml
+
 def healthcheck_timeouts(interval=10, timeout=10, retries=5, start_period=30):
-  return textwrap.indent(f"""
-    interval: {interval}s
-    timeout: {timeout}s
-    retries: {retries}
-    start_period: {start_period}s
-  """, 2 * " ")
+  timeouts = {
+    "interval": interval,
+    "timeout": timeout,
+    "retries": retries,
+    "start_period": start_period
+  }
+
+  return yaml.dump(timeouts)
