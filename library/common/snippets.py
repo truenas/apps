6ddf675bcbb436bf56a8132c06381e5dbff02aec
Stavros kois <s.kois@outlook.com>
2024-04-01 19:04:05 +0300

    more snipepts

--- library/common/snippets.py
+++ library/common/snippets.py
@@ -6,24 +6,47 @@ YAML_OPTS = {
   'indent': 2,
 }
 
-def healthcheck(test="", interval=10, timeout=10, retries=5, start_period=30):
+def health_check(test="", interval=10, timeout=10, retries=5, start_period=30):
   if not test:
     raise ValueError("Healtcheck: [test] must be set")
 
-  timeouts = {
-    "healthcheck": {
-      "test": test,
-      "interval": f'{interval}s',
-      "timeout": f'{timeout}s',
-      "retries": retries,
-      "start_period": f'{start_period}s'
-    }
+  hc = {
+    "test": test,
+    "interval": f'{interval}s',
+    "timeout": f'{timeout}s',
+    "retries": retries,
+    "start_period": f'{start_period}s'
   }
 
-  return yaml.dump(timeouts, **YAML_OPTS)
+  return yaml.dump(hc, **YAML_OPTS)
 
 def curl_test(url):
   return f"curl --silent --fail {url}"
 
-def pg_test(host, user, db, port=5432):
+def pg_test(user, db, host="127.0.0.1", port=5432):
+  if not user:
+    raise ValueError("Postgres container: [user] must be set")
+
+  if not db:
+    raise ValueError("Postgres container: [db] must be set")
+
   return f"pg_isready -h {host} -p {port} -d {db} -U {user}"
+
+def postgres_run_as():
+  return "999:999"
+
+def postgres_environment(user, password, db):
+  if not user:
+    raise ValueError("Postgres container: [user] must be set")
+
+  if not password:
+    raise ValueError("Postgres container: [password] must be set")
+
+  if not db:
+    raise ValueError("Postgres container: [db] must be set")
+
+  return {
+    "POSTGRES_USER": user,
+    "POSTGRES_PASSWORD": password,
+    "POSTGRES_DB": db
+  }
