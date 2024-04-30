25d167da808cc2ee46670b0555de103a8b4433cc
Stavros kois <s.kois@outlook.com>
2024-04-30 21:05:21 +0300

    add some todo

--- library/common/utils.py
+++ library/common/utils.py
@@ -15,16 +15,16 @@ def func_secure_string(length):
   return secrets.token_urlsafe(length)
 
 # TODO: maybe extend this with ACLs API?!
+# TODO: once we have ixVolumes in update this func
 def func_host_path_with_perms(path, perms):
   if not path:
     raise ValueError("path is required")
 
-  if not perms.get('user') or not perms.get('group'):
-    raise ValueError("user and group are required")
-
   os.makedirs(path, exist_ok=True)
 
-  # Set permissions
-  os.chown(path, int(perms['user']), int(perms['group']))
+  # FIXME: only do such things if user agreed or if its ixVolumes
+  if perms.get('user') and perms.get('group'):
+    # Set permissions
+    os.chown(path, int(perms['user']), int(perms['group']))
 
   return ''
