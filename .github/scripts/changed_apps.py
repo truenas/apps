62952a39b112683dd02dc3b7388ee51b4f85dc81
Stavros kois <s.kois@outlook.com>
2024-05-10 23:09:05 +0300

    fix

--- .github/scripts/changed_apps.py
+++ .github/scripts/changed_apps.py
@@ -6,7 +6,10 @@ import sys
 import os
 import re
 
-APP_REGEX = re.compile(r"^ix-dev/(.+)/(.+)")
+# Train and App name can contain:
+# - alphanumeric characters
+# - dots, dashes and underscores
+APP_REGEX = re.compile(r"^ix-dev\/([-\w\.]+)\/([-\w\.]+)")
 TEST_VALUES_DIR = "test_values"
 
 # Get the changed files (json formatted)
@@ -15,10 +18,8 @@ if json_files == "":
     print("Environment variable CHANGED_FILES is empty", file=sys.stderr)
     exit(1)
 
-print(f"Before: {json_files}", file=sys.stderr)
 # Remove escaped backslashes coming from shell
 json_files = json_files.replace("\\", "")
-print(f"After: {json_files}", file=sys.stderr)
 
 # Print to stderr, in order to keep stdout only for data
 print(f"Changed files: {json_files}", file=sys.stderr)
@@ -26,19 +27,17 @@ print(f"Changed files: {json_files}", file=sys.stderr)
 # Parse the json
 changed_files = json.loads(json_files)
 
-
 result = []
 for file in changed_files:
     match = APP_REGEX.match(file)
-
     if not match:
-        pass
+        continue
 
     item = {
         "train": match.group(1),
         "app": match.group(2),
         "test_values": [
-            file
+            file.name
             for file in pathlib.Path(
                 # ix-dev/{train}/{app}/test_values/*.yaml
                 "ix-dev",
