050835f7ff6e8efa2f1b3c405651fb14868a9262
Stavros kois <s.kois@outlook.com>
2024-05-14 22:59:53 +0300

    we need to print not exec!

--- library/1.0.0/permissions.py
+++ library/1.0.0/permissions.py
@@ -1,45 +1,53 @@
 from . import utils
-import pathlib
+import textwrap
 import re
-import os
 
 RE_ID = re.compile(r"^[0-9]+$")
 RE_MODE = re.compile(r"^(0o)?([0-7]{3})$")
 
 
-def chown(path, uid, gid, force=False):
-    if not pathlib.Path(path).exists():
-        utils.throw_error(f"Path [{path}] does not exist")
+def check_path(path):
+    out = f"""
+    if not pathlib.Path({path}).exists():
+        raise Exception(f"Path [{path}] does not exist")
+    """
+    return textwrap.dedent(out)
 
-    if any(pathlib.Path(path).iterdir()):
-        if not force:
-            utils.throw_error(
+
+def check_empty(path, force=False):
+    out = f"""
+    if any(pathlib.Path({path}).iterdir()):
+        if not {force}:
+            raise Exception(
                 f"Path [{path}] is not empty, skipping... Use [force=True] to override"
             )
+    """
+    return textwrap.dedent(out)
 
-    if not RE_ID.match(uid):
+
+def chown(path, uid, gid, force=False):
+    if not RE_ID.match(str(uid)):
         utils.throw_error(f"User ID must be a number, but got [{uid}]")
-    if not RE_ID.match(gid):
+    if not RE_ID.match(str(gid)):
         utils.throw_error(f"Group ID must be a number, but got [{gid}]")
 
-    os.chown(path, int(uid), int(gid))
-    return ""
+    out = f"""
+    {check_path(path)}
+    {check_empty(path, force)}
+    os.chown({path}, {int(uid)}, {int(gid)})
+    """
+    return textwrap.dedent(out)
 
 
 def chmod(path, mode, force=False):
-    if not pathlib.Path(path).exists():
-        utils.throw_error(f"Path [{path}] does not exist")
-
-    if any(pathlib.Path(path).iterdir()):
-        if not force:
-            utils.throw_error(
-                f"Path [{path}] is not empty, skipping... Use [force=True] to override"
-            )
-
     if not RE_MODE.match(mode):
         utils.throw_error(
             f"Mode must be in octal format, but got [{mode}]. Example: 0o755 or 755"
         )
 
-    os.chmod(path, int(mode, 8))
-    return ""
+    out = f"""
+    {check_path(path)}
+    {check_empty(path, force)}
+    os.chmod({path}, {int(mode, 8)})
+    """
+    return textwrap.dedent(out)
