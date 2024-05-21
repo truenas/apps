2fb7d897fe4cd5f170c89049f6162fe4ccd28ae9
Stavros kois <s.kois@outlook.com>
2024-05-21 17:48:45 +0300

    update lib stuff

--- library/1.0.0/validations.py
+++ library/1.0.0/validations.py
@@ -1,45 +1,4 @@
 from . import utils
-import re
-
-
-def is_email(email):
-    return re.match(r"[^@]+@[^@]+\.[^@]+", email)
-
-
-def must_be_email(email):
-    if not is_email(email):
-        utils.throw_error("Value must be an email address")
-
-    return email
-
-
-def must_be_length(value, length):
-    if len(value) < length:
-        utils.throw_error(f"Value must be {length} characters long")
-
-    return value
-
-
-def is_password_secure(password):
-    checks = [
-        lambda p: len(p) >= 8,
-        lambda p: re.search("[a-z]", p),
-        lambda p: re.search("[A-Z]", p),
-        lambda p: re.search("[0-9]", p),
-        lambda p: re.search('[!@#$%^&*(),.?":{}|<>]', p),
-    ]
-
-    return all(c(password) for c in checks)
-
-
-def must_be_password_secure(password):
-    if not is_password_secure(password):
-        utils.throw_error(
-            "Password must contain at least 8 characters, 1 uppercase letter,"
-            + " 1 lowercase letter, 1 number, and 1 special character"
-        )
-
-    return password
 
 
 def validate_path(path):
