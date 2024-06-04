2f79750d60ce9cc9b4719d69be7b0630aefd43e2
Stavros kois <s.kois@outlook.com>
2024-06-04 17:46:58 +0300

    extend lib

---
+++ library/1.0.0/environment.py
@@ -0,0 +1,28 @@
+from . import utils
+
+
+def envs(app={}, user=[]):
+    track_env = {**app}
+    result = {}
+
+    if not user:
+        user = []
+    elif isinstance(user, dict):
+        user = user.items()
+    elif isinstance(user, list):
+        user = user
+    else:
+        utils.throw_error(f"Unsupported type for user environment variables [{type(user)}]")
+
+    for k in app.keys():
+        if not k:
+            utils.throw_error("Environment variable name cannot be empty.")
+
+    for item in user:
+        if not item.get("name", None):
+            utils.throw_error("Environment variable name cannot be empty.")
+        if item.get("name", None) in track_env:
+            utils.throw_error(f"Environment variable [{k}] is already defined from the application developer.")
+        track_env[item["name"]] = item.get("value", None)
+        result[item["name"]] = item.get("value", None)
+    return result
