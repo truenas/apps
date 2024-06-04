f13ee501d4bc6b4b9985655db5467c72e132b81a
Stavros kois <s.kois@outlook.com>
2024-06-04 17:46:58 +0300

    extend lib

---
+++ library/1.0.0/security.py
@@ -0,0 +1,16 @@
+def get_caps(add=[], drop=[]):
+    result = {"cap_drop": drop or ["ALL"]}
+    if add:
+        result["cap_add"] = add
+    return result
+
+
+def get_sec_opts(add=[], remove=[]):
+    result = ["no-new-privileges", "seccomp=builtin"]
+    for opt in add:
+        if opt not in result:
+            result.append(opt)
+    for opt in remove:
+        if opt in result:
+            result.remove(opt)
+    return result
