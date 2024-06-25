8b551aac0964af8d6767aa1438d91446869dcd87
Stavros kois <s.kois@outlook.com>
2024-06-25 22:07:46 +0300

    update lib and hashes

---
+++ ix-dev/community/readarr/templates/library/base_v1_0_0/utils.py
@@ -0,0 +1,18 @@
+import secrets
+import sys
+
+
+class TemplateException(Exception):
+    pass
+
+
+def throw_error(message):
+    # When throwing a known error, hide the traceback
+    # This is because the error is also shown in the UI
+    # and having a traceback makes it hard for user to read
+    sys.tracebacklimit = 0
+    raise TemplateException(message)
+
+
+def secure_string(length):
+    return secrets.token_urlsafe(length)
