49ad1672805acf15d3531673154a48f461b77685
Stavros kois <s.kois@outlook.com>
2024-06-27 18:37:18 +0300

    2 validators

--- ix-dev/community/ddns-updater/templates/library/community/ddns-updater/v1_0_0/config.py
+++ ix-dev/community/ddns-updater/templates/library/community/ddns-updater/v1_0_0/config.py
@@ -1,8 +1,37 @@
 from base_v1_0_0 import utils
 
 
-def get_public_ip_providers(items=[]):
-    result = []
+def validate_public_ip_dns_providers(items=[]):
+    valid_providers = [
+        "cloudflare",
+        "opendns",
+    ]
+
+    for item in items:
+        if not item.get("provider"):
+            utils.throw_error("Expected [provider] to be set for public ip provider")
+        if item["provider"] == "custom":
+            utils.throw_error("Custom provider is not supported for public ip dns providers")
+        if item["provider"] not in valid_providers:
+            utils.throw_error(f"Expected [provider] to be one of [{', '.join(valid_providers)}], got [{item['provider']}]")
+
+
+def validate_public_ip_http_providers(items=[]):
+    valid_providers = [
+        "custom",
+        "ipify",
+        "ifconfig",
+        "ipinfo",
+        "google",
+        "spdyn",
+        "ipleak",
+        "icanhazip",
+        "ident",
+        "nnev",
+        "wtfismyip",
+        "seeip",
+        "changeip",
+    ]
 
     for item in items:
         if not item.get("provider"):
@@ -10,6 +39,28 @@ def get_public_ip_providers(items=[]):
         if item["provider"] == "custom":
             if not item.get("custom"):
                 utils.throw_error("Expected [custom] to be set when public ip provider is [custom]")
+            if not item["custom"].startswith("url:"):
+                utils.throw_error("Expected [custom] to start with [url:]")
+        if item["provider"] not in valid_providers:
+            utils.throw_error(f"Expected [provider] to be one of [{', '.join(valid_providers)}], got [{item['provider']}]")
+
+
+def get_public_ip_providers(category: str, items=[]):
+    result = []
+
+    if category == "PUBLICIP_DNS_PROVIDERS":
+        validate_public_ip_dns_providers(items)
+    elif category == "PUBLICIP_HTTP_PROVIDERS":
+        validate_public_ip_http_providers(items)
+    # elif category == "PUBLICIPV4_HTTP_PROVIDERS":
+    #     validate_public_ipv4_http_providers(items)
+    # elif category == "PUBLICIPV6_HTTP_PROVIDERS":
+    #     validate_public_ipv6_http_providers(items)
+    # elif category == "PUBLICIP_FETCHERS":
+    #     validate_public_ip_fetchers(items)
+
+    for item in items:
+        if item["provider"] == "custom":
             result.append(item["custom"])
         else:
             result.append(item["provider"])
