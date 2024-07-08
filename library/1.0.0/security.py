ec88d0434b33bd68c2570d2f7de9bd86b33f4b9b
Stavros kois <s.kois@outlook.com>
2024-07-08 21:56:24 +0300

    update lib

--- library/1.0.0/security.py
+++ library/1.0.0/security.py
@@ -1,3 +1,6 @@
+from base64 import b64encode
+
+
 def get_caps(add=[], drop=[]):
     result = {"drop": drop or ["ALL"]}
     if add:
@@ -14,3 +17,7 @@ def get_sec_opts(add=[], remove=[]):
         if opt in result:
             result.remove(opt)
     return result
+
+
+def htpasswd(username, password):
+    return b64encode(f"{username}:{password}".encode("utf-8")).decode("utf-8")
