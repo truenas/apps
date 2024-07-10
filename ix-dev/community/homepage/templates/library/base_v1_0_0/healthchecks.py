7a4e9648e747d20da7f624d7486dfb5c5c5c15be
Stavros kois <s.kois@outlook.com>
2024-07-10 20:40:14 +0300

    add homepage

---
+++ ix-dev/community/homepage/templates/library/base_v1_0_0/healthchecks.py
@@ -0,0 +1,82 @@
+from . import utils
+
+
+def check_health(test, interval=10, timeout=10, retries=5, start_period=30):
+    if not test:
+        utils.throw_error("Expected [test] to be set")
+
+    return {
+        "test": test,
+        "interval": f"{interval}s",
+        "timeout": f"{timeout}s",
+        "retries": retries,
+        "start_period": f"{start_period}s",
+    }
+
+
+def pg_test(user, db, config={}):
+    if not user or not db:
+        utils.throw_error("Postgres container: [user] and [db] must be set")
+
+    host = config.get("host", "127.0.0.1")
+    port = config.get("port", 5432)
+
+    return f"pg_isready -h {host} -p {port} -d {db} -U {user}"
+
+
+def curl_test(port, path, config={}):
+    if not port or not path:
+        utils.throw_error("Expected [port] and [path] to be set")
+
+    scheme = config.get("scheme", "http")
+    host = config.get("host", "127.0.0.1")
+    headers = config.get("headers", [])
+
+    opts = []
+    if scheme == "https":
+        opts.append("--insecure")
+
+    for header in headers:
+        if not header[0] or not header[1]:
+            utils.throw_error("Expected [header] to be a list of two items")
+        opts.append(f'--header "{header[0]}: {header[1]}"')
+
+    return f"curl --silent --output /dev/null --show-error --fail {' '.join(opts)} {scheme}://{host}:{port}{path}"
+
+
+def wget_test(port, path, config={}):
+    if not port or not path:
+        utils.throw_error("Expected [port] and [path] to be set")
+
+    scheme = config.get("scheme", "http")
+    host = config.get("host", "127.0.0.1")
+    headers = config.get("headers", [])
+
+    opts = []
+    if scheme == "https":
+        opts.append("--no-check-certificate")
+
+    for header in headers:
+        if not header[0] or not header[1]:
+            utils.throw_error("Expected [header] to be a list of two items")
+        opts.append(f'--header "{header[0]}: {header[1]}"')
+
+    return f"wget --spider --quiet {' '.join(opts)} {scheme}://{host}:{port}{path}"
+
+
+def http_test(port, path, config={}):
+    if not port or not path:
+        utils.throw_error("Expected [port] and [path] to be set")
+
+    host = config.get("host", "127.0.0.1")
+
+    return f"/bin/bash -c 'exec {{health_check_fd}}<>/dev/tcp/{host}/{port} && echo -e \"GET {path} HTTP/1.1\\r\\nHost: {host}\\r\\nConnection: close\\r\\n\\r\\n\" >&$${{health_check_fd}} && cat <&$${{health_check_fd}}'"
+
+
+def tcp_test(port, config={}):
+    if not port:
+        utils.throw_error("Expected [port] to be set")
+
+    host = config.get("host", "127.0.0.1")
+
+    return f"nc -z -w 1 {host} {port}"
