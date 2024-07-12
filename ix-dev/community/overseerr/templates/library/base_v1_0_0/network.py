f46fc7f1743dd903b6dcef8e5057b5e9df81de19
Stavros kois <s.kois@outlook.com>
2024-07-12 17:13:47 +0300

    update lib

--- ix-dev/community/overseerr/templates/library/base_v1_0_0/network.py
+++ ix-dev/community/overseerr/templates/library/base_v1_0_0/network.py
@@ -10,7 +10,9 @@ def dns_opts(dns_opts=[]):
     for opt in dns_opts:
         key = opt.split(":")[0]
         if key in tracked:
-            utils.throw_error(f"Expected [dns_opts] to be unique, got [{', '.join([d.split(':')[0] for d in tracked])}]")
+            utils.throw_error(
+                f"Expected [dns_opts] to be unique, got [{', '.join([d.split(':')[0] for d in tracked])}]"
+            )
         if key in disallowed_opts:
             utils.throw_error(f"Expected [dns_opts] to not contain [{key}] key.")
         tracked[key] = opt
