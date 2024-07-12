8d1d7586b38cc60f54c470705b0968f5879fddde
Stavros kois <s.kois@outlook.com>
2024-07-12 17:20:17 +0300

    lint

--- ix-dev/enterprise/minio/templates/library/enterprise/minio/v1_0_0/data.py
+++ ix-dev/enterprise/minio/templates/library/enterprise/minio/v1_0_0/data.py
@@ -8,21 +8,30 @@ def validate(data):
         utils.throw_error("At least 1 storage item must be set")
 
     if len(storage["data_dirs"]) > 1 and not len(multi_mode.get("entries", [])) > 0:
-        utils.throw_error("[Multi Mode] must be enabled if more than 1 storage item is set")
+        utils.throw_error(
+            "[Multi Mode] must be enabled if more than 1 storage item is set"
+        )
 
     # make sure mount_paths in data["storage"]["data_dirs"] are unique
     mount_paths = [item["mount_path"] for item in storage["data_dirs"]]
     if len(mount_paths) != len(set(mount_paths)):
-        utils.throw_error(f"Mount paths in storage items must be unique, found duplicates: [{', '.join(mount_paths)}]")
+        utils.throw_error(
+            f"Mount paths in storage items must be unique, found duplicates: [{', '.join(mount_paths)}]"
+        )
 
     if len(multi_mode.get("entries", [])) > 0:
         disallowed_keys = ["server"]
         for item in multi_mode["entries"]:
             if item in disallowed_keys:
-                utils.throw_error(f"MinIO: Value [{item}] is not allowed in [Multi Mode] items")
+                utils.throw_error(
+                    f"MinIO: Value [{item}] is not allowed in [Multi Mode] items"
+                )
 
             # /data{1...4}
             if item.startswith("/"):
                 # check if these characters exist in item
                 if any(char in item for char in ["{", "}"]) and "..." not in item:
-                    utils.throw_error("MinIO: [Multi Mode] items must have 3 dots when they are paths with expansion eg [/some_path{1...4}]")
+                    utils.throw_error(
+                        "MinIO: [Multi Mode] items must have 3 dots when they are paths"
+                        + " with expansion eg [/some_path{1...4}]"
+                    )
