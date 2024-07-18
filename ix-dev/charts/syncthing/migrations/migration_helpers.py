ca5a0be5fff43e81765c37258be7b46dbf9d29f1
Stavros kois <s.kois@outlook.com>
2024-07-18 22:02:19 +0300

    handle some things better

--- ix-dev/charts/syncthing/migrations/migration_helpers.py
+++ ix-dev/charts/syncthing/migrations/migration_helpers.py
@@ -91,7 +91,7 @@ def migrate_dns_config(dns_config):
 
     dns_opts = []
     for opt in dns_config.get("options", []):
-        dns_opts.append(f"{opt.key}:{opt.value}")
+        dns_opts.append(f"{opt['name']}:{opt['value']}")
 
     return dns_opts
 
@@ -117,7 +117,7 @@ def migrate_acl_entries(acl_entries: dict) -> dict:
 def migrate_ix_volume_type(ix_volume):
     vol_config = ix_volume.get("ixVolumeConfig", {})
     if not vol_config:
-        return {}
+        raise ValueError("Expected [ix_volume] to have [ixVolumeConfig] set")
 
     result = {
         "type": "ix_volume",
@@ -138,35 +138,34 @@ def migrate_ix_volume_type(ix_volume):
 def migrate_host_path_type(host_path):
     path_config = host_path.get("hostPathConfig", {})
     if not path_config:
-        return {}
+        raise ValueError("Expected [host_path] to have [hostPathConfig] set")
 
     result = {
         "type": "host_path",
         "host_path_config": {
-            "acl_enable": host_path.get("aclEnable", False),
+            "acl_enable": path_config.get("aclEnable", False),
         },
     }
 
-    if host_path.get("aclEnable", False):
+    if path_config.get("aclEnable", False):
         result["host_path_config"].update(
-            {"acl": migrate_acl_entries(host_path.get("acl", {}))}
+            {"acl": migrate_acl_entries(path_config.get("acl", {}))}
         )
     else:
-        result["host_path_config"].update({"host_path": host_path["hostPath"]})
+        result["host_path_config"].update({"host_path": path_config["hostPath"]})
 
     return result
 
 
 def migrate_storage_item(storage_item):
     if not storage_item:
-        return {}
+        raise ValueError("Expected [storage_item] to be set")
 
     result = {}
     if storage_item["type"] == "ixVolume":
         result = migrate_ix_volume_type(storage_item)
     elif storage_item["type"] == "hostPath":
-        pass
-        # return migrate_host_path_type(storage_item)
+        result = migrate_host_path_type(storage_item)
 
     mount_path = storage_item.get("mountPath", "")
     if mount_path:
