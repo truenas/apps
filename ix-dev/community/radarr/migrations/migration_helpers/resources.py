4ce386835dfb3557a7749567c0f4bb90e672f40d
Stavros kois <s.kois@outlook.com>
2024-07-24 20:59:41 +0300

    update helpers

--- ix-dev/community/radarr/migrations/migration_helpers/resources.py
+++ ix-dev/community/radarr/migrations/migration_helpers/resources.py
@@ -2,25 +2,13 @@ from .memory import transform_memory, TOTAL_MEM
 from .cpu import transform_cpu, CPU_COUNT
 
 
-<<<<<<< HEAD
 def migrate_resources(resources, gpus=None):
     gpus = gpus or {}
-=======
-def migrate_resources(resources):
-    # Handle empty resources, with sane defaults
-    if not resources:
-        return {
-            "limits": {
-                "cpus": (CPU_COUNT or 2) / 2,
-                "memory": f"{TOTAL_MEM / 1024 / 1024}M",
-            }
-        }
->>>>>>> 656f33d4 (handle case that os.cpu_count is zero)
 
     result = {
         "limits": {
             "cpus": (CPU_COUNT or 2) / 2,
-            "memory": f"{TOTAL_MEM / 1024 / 1024}M",
+            "memory": {TOTAL_MEM / 1024 / 1024},
         }
     }
 
