851f4cb0d33f0ec8fd3f920d6a4aff3c73e2a783
Stavros kois <s.kois@outlook.com>
2024-08-13 18:16:15 +0300

    initial renovate setup

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  extends: [],
   // https://docs.renovatebot.com/self-hosted-configuration/#dryrun
   dryRun: false,
   // https://docs.renovatebot.com/configuration-options/#gitauthor
-  gitAuthor: "ix-bot",
+  gitAuthor: "ix-bot <ix-bot@users.noreply.github.com>",
   // https://docs.renovatebot.com/self-hosted-configuration/#onboarding
   onboarding: false,
   // https://docs.renovatebot.com/self-hosted-configuration/#platform
@@ -14,26 +15,47 @@ module.exports = {
   // https://docs.renovatebot.com/self-hosted-configuration/#allowedpostupgradecommands
   // TODO: Restrict this.
   allowedPostUpgradeCommands: ["^.*"],
+  enabledManagers: ["regex", "github-actions"],
   customManagers: [
     {
-      fileMatch: ["^.*ix_values\\.yaml$"],
+      // Match only ix_values.yaml files in the ix-dev directory
+      fileMatch: ["^ix-dev/.*/ix_values\\.yaml$"],
+      // Matches the repository name and the tag of each image
       matchStrings: [
-        "\\s{4}repository: (?<depName>[^\\s]+)\\n\\s{4}tag: (?<currentValue>[^\\s]+)",
+        '\\s{4}repository: (?<depName>[^\\s]+)\\n\\s{4}tag: "?(?<currentValue>[^\\s"]+)"?',
       ],
+      // Use the docker datasource on matched images
       datasourceTemplate: "docker",
     },
   ],
   packageRules: [
     {
+      matchManagers: ["regex"],
       matchDatasources: ["docker"],
-      matchUpdateTypes: ["major"],
       postUpgradeTasks: {
+        // What to "git add" after the commands are run
         fileFilters: ["**/app.yaml"],
         executionMode: "branch",
         commands: [
-          "echo {{{packageFileDir}}}, {{{depName}}}, {{{currentValue}}} - {{{newValue}}}",
+          // See what we get. TODO: come back and add a script to bump app.yaml (and the app lib if any)
+          "echo {{{packageFileDir}}}, {{{depName}}}, {{{currentValue}}} - {{{newValue}}} >> ./renovate.log",
         ],
       },
+    },
+    {
+      matchDatasources: ["docker"],
+      matchUpdateTypes: ["major"],
+      labels: ["major"],
+    },
+    {
+      matchDatasources: ["docker"],
+      matchUpdateTypes: ["minor"],
+      labels: ["minor"],
+    },
+    {
+      matchDatasources: ["docker"],
+      matchUpdateTypes: ["patch"],
+      labels: ["patch"],
     },
   ],
 };
