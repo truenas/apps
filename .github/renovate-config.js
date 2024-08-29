9c0557b6f510d25ea5b2bb7a618001c04c9f4118
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2024-08-29 21:33:13 +0300

    Renovate workflow (#122)
    * add renovate
    
    * initial renovate setup
    
    * empty file to enable the bot
    
    * clarify
    
    * newline
    
    * update groups
    
    * update excecutionMode
    
    * bump app once, even if multiple deps are changed
    
    * update filters
    
    * restrict more
    
    * use bugclerk as author
    
    * mount socket and add script

---
+++ .github/renovate-config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  extends: [],
+  // https://docs.renovatebot.com/self-hosted-configuration/#dryrun
+  dryRun: false,
+  // https://docs.renovatebot.com/configuration-options/#gitauthor
+  gitAuthor: "bugclerk <bugclerk@ixsystems.com>",
+  // https://docs.renovatebot.com/self-hosted-configuration/#onboarding
+  onboarding: false,
+  // https://docs.renovatebot.com/configuration-options/#dependencydashboard
+  dependencyDashboard: true,
+  // https://docs.renovatebot.com/self-hosted-configuration/#platform
+  platform: "github",
+  // https://docs.renovatebot.com/self-hosted-configuration/#repositories
+  repositories: ["truenas/apps"],
+  // https://docs.renovatebot.com/self-hosted-configuration/#allowpostupgradecommandtemplating
+  allowPostUpgradeCommandTemplating: true,
+  // https://docs.renovatebot.com/self-hosted-configuration/#allowedpostupgradecommands
+  // TODO: Restrict this.
+  allowedPostUpgradeCommands: ["^.*"],
+  enabledManagers: ["regex", "github-actions"],
+  customManagers: [
+    {
+      // Match only ix_values.yaml files in the ix-dev directory
+      fileMatch: ["^ix-dev/.*/ix_values\\.yaml$"],
+      // Matches the repository name and the tag of each image
+      matchStrings: [
+        '\\s{4}repository: (?<depName>[^\\s]+)\\n\\s{4}tag: "?(?<currentValue>[^\\s"]+)"?',
+      ],
+      // Use the docker datasource on matched images
+      datasourceTemplate: "docker",
+    },
+  ],
+  packageRules: [
+    {
+      matchManagers: ["regex"],
+      matchDatasources: ["docker"],
+      postUpgradeTasks: {
+        // What to "git add" after the commands are run
+        fileFilters: [
+          "ix-dev/**/app.yaml", // For the version update
+          "ix-dev/**/templates/**", // For the app lib versioned dir
+        ],
+        // Execute the following commands for every dep.
+        executionMode: "update",
+        commands: [
+          "./.github/scripts/renovate_bump.sh {{{packageFileDir}}} {{{updateType}}}",
+        ],
+      },
+    },
+    {
+      matchDatasources: ["docker"],
+      matchUpdateTypes: ["major"],
+      labels: ["major"],
+    },
+    {
+      matchDatasources: ["docker"],
+      matchUpdateTypes: ["minor"],
+      groupName: "updates-patch-minor",
+      labels: ["minor"],
+    },
+    {
+      matchDatasources: ["docker"],
+      matchUpdateTypes: ["patch"],
+      groupName: "updates-patch-minor",
+      labels: ["patch"],
+    },
+  ],
+};
