a26f7dab152823af0e7f1c1137e1a56d2d48556b
Stavros kois <s.kois@outlook.com>
2024-09-04 16:59:33 +0300

    fix

--- ix-dev/community/invidious/templates/macros/config.sh
+++ ix-dev/community/invidious/templates/macros/config.sh
@@ -1,12 +1,18 @@
 {% macro config(values, cfg=[]) -%}
 #!/bin/sh
 {%- set cfg_path = "%s/config.yaml"|format(values.consts.config_path) %}
-echo "Updating [{{ cfg_path }}] file..."
+if [ ! -f "{{ cfg_path }}" ]; then
+  echo "File [{{ cfg_path }}] does not exist. Exiting..."
+  exit 1
+fi
 
+echo "Updating [{{ cfg_path }}] file..."
 {%- for c in cfg %}
-echo "Updating [{{c.key}}] key..."
-yq -i '.{{ c.key }} = {{ c.value }}' "{{ cfg_path }}"
-echo "New value for [{{c.key}}]: $$(yq '.{{ c.key }}' "{{ cfg_path }}")"
+{%- for key, value in c.items() %}
+echo "Updating [{{ key }}] key..."
+yq -i '.{{ key }} = {{ value }}' "{{ cfg_path }}"
+echo "New value for [{{ key }}]: $$(yq '.{{ key }}' "{{ cfg_path }}")"
+{%- endfor %}
 {%- endfor %}
 echo "Done!"
 
