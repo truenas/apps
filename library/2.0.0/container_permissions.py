5f05c90bd058bccb902dc54df32358f1a08dad34
Stavros kois <s.kois@outlook.com>
2024-10-15 22:52:36 +0300

    add some todo

--- library/2.0.0/container_permissions.py
+++ library/2.0.0/container_permissions.py
@@ -8,6 +8,8 @@ if TYPE_CHECKING:
 class ContainerPermissions:
     def __init__(self, render_instance: "Render"):
         self._render_instance = render_instance
+        # TODO: check here if the permissions container is needed.
+        # probably in the render_instance have a dict with the info needed.
 
     def add_container(self):
         c = self._render_instance.add_container("permissions", "python_permissions_image")
@@ -15,6 +17,7 @@ class ContainerPermissions:
         c.set_user(0, 0)
         c.add_caps(["CHOWN", "FOWNER", "DAC_OVERRIDE"])
 
+        # Don't attach any devices
         c.deploy.resources.remove_devices()
         c.deploy.resources.set_profile("medium")
         c.healthcheck.disable()
@@ -25,6 +28,12 @@ class ContainerPermissions:
         script = """
         #!/usr/bin/env python3
         import os
+        import json
+
+        with open("/script/actions.json", "r") as f:
+            actions_data = json.load(f)
+        print(actions_data)
+
         # TODO:
         """
 
