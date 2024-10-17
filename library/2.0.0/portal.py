52b808cf20e146c7c0167dc1a8b0c6f5b2f7c705
Stavros kois <s.kois@outlook.com>
2024-10-17 17:14:13 +0300

    make sure host is not empty

--- library/2.0.0/portal.py
+++ library/2.0.0/portal.py
@@ -8,7 +8,7 @@ class Portal:
     def __init__(self, name: str, config: dict):
         self._name = name
         self._scheme = valid_portal_scheme_or_raise(config.get("scheme", "http"))
-        self._host = config.get("host", "0.0.0.0")
+        self._host = config.get("host", "0.0.0.0") or "0.0.0.0"
         self._port = valid_port_or_raise(config.get("port", 0))
         self._path = valid_http_path_or_raise(config.get("path", "/"))
 
