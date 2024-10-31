194a298348782b9fc2000fc26255e8901cb9695c
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2024-10-31 18:49:24 +0200

    v2-lib: fix $ escaping (#759)
    * auto add labels from a top level structure
    
    * improve message
    
    * fix escaping
    
    * fix escaping
    
    * clean

--- library/2.0.5/formatter.py
+++ library/2.0.5/formatter.py
@@ -1,15 +1,9 @@
-import re
 import json
 import hashlib
 
 
-# Replaces all single dollar signs with double dollar signs
-# Docker tries to expand shell variables, so we need to
-# escape them in multiple places
-# It will not replace dollar signs that are already escaped
-def escape_dollar(text):
-    # https://regex101.com/r/tdbI7y/1
-    return re.sub(r"(?<!\$)\$(?!\$)", r"$$", text)
+def escape_dollar(text: str) -> str:
+    return text.replace("$", "$$")
 
 
 def get_hashed_name_for_volume(prefix: str, config: dict):
