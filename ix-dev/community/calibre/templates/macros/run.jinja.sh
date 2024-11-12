80a1e78558adffad9de82572f46e41b93837d9d5
Stavros kois <s.kois@outlook.com>
2024-11-12 20:29:03 +0200

    move things around

--- ix-dev/community/calibre/templates/macros/run.jinja.sh
+++ ix-dev/community/calibre/templates/macros/run.jinja.sh
@@ -1,18 +1,13 @@
 {% macro run(values) -%}
-{%- set user_db = "%s/users.db" | format(values.consts.config_path) -%}
 {%- set flags = namespace(x=[
   "--port=%d"|format(values.network.web_port),
   "--enable-local-write",
   "--disable-use-bonjour",
-  "--trusted-ips=192.168.0.0/16,172.16.0.0/12,10.0.0.0/8",
+  "--trusted-ips=%s"|format(values.calibre.trusted_ips|join(",")),
 ]) -%}
 
-if [ ! -f {{ values.consts.media_path }}/metadata.db ]; then
-  echo 'Creating an empty calibre metadata database at [{{ values.consts.media_path }}/metadata.db]'
-  touch {{ values.consts.media_path }}/metadata.db
-fi
-
 {%- if values.calibre.enable_auth -%}
+  {%- set user_db = "%s/users.db" | format(values.consts.config_path) -%}
   {%- do flags.x.append("--enable-auth") -%}
   {%- do flags.x.append("--userdb=%s"|format(user_db)) %}
 
@@ -23,6 +18,11 @@ if [ ! -f {{ user_db }} ]; then
     --manage-users add admin '{{ values.consts.default_admin_pass }}'
 fi
 {%- endif %}
+
+if [ ! -f {{ values.consts.media_path }}/metadata.db ]; then
+  echo 'Creating an empty calibre metadata database at [{{ values.consts.media_path }}/metadata.db]'
+  touch {{ values.consts.media_path }}/metadata.db
+fi
 
 /usr/bin/calibre-server {{ flags.x | join(" ") }} {{ values.consts.media_path }}
 {% endmacro %}
