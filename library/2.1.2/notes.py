506cd791b95131c89e9e8176e9d9019cf2967f67
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2024-12-18 16:26:28 +0200

    library: update notes (#1181)

--- library/2.1.2/notes.py
+++ library/2.1.2/notes.py
@@ -8,6 +8,7 @@ class Notes:
     def __init__(self, render_instance: "Render"):
         self._render_instance = render_instance
         self._app_name: str = ""
+        self._app_train: str = ""
         self._warnings: list[str] = []
         self._deprecations: list[str] = []
         self._header: str = ""
@@ -15,27 +16,32 @@ class Notes:
         self._footer: str = ""
 
         self._auto_set_app_name()
+        self._auto_set_app_train()
         self._auto_set_header()
         self._auto_set_footer()
 
+    def _is_enterprise_train(self):
+        if self._app_train == "enterprise":
+            return True
+
     def _auto_set_app_name(self):
-        app_name = self._render_instance.values.get("ix_context", {}).get("app_metadata", {}).get("name", "")
+        app_name = self._render_instance.values.get("ix_context", {}).get("app_metadata", {}).get("title", "")
         self._app_name = app_name or "<app_name>"
 
+    def _auto_set_app_train(self):
+        app_train = self._render_instance.values.get("ix_context", {}).get("app_metadata", {}).get("train", "")
+        self._app_train = app_train or "<app_train>"
+
     def _auto_set_header(self):
-        head = "# Welcome to TrueNAS SCALE\n\n"
-        head += f"Thank you for installing {self._app_name}!\n\n"
-        self._header = head
+        self._header = f"# {self._app_name}\n\n"
 
     def _auto_set_footer(self):
-        footer = "## Documentation\n\n"
-        footer += f"Documentation for {self._app_name} can be found at https://www.truenas.com/docs.\n\n"
-        footer += "## Bug reports\n\n"
-        footer += "If you find a bug in this app, please file an issue at\n"
-        footer += "https://ixsystems.atlassian.net or https://github.com/truenas/apps\n\n"
-        footer += "## Feature requests or improvements\n\n"
-        footer += "If you find a feature request for this app, please file an issue at\n"
-        footer += "https://ixsystems.atlassian.net or https://github.com/truenas/apps\n"
+        url = "https://github.com/truenas/apps"
+        if self._is_enterprise_train():
+            url = "https://ixsystems.atlassian.net"
+        footer = "## Bug Reports and Feature Requests\n\n"
+        footer += "If you find a bug in this app or have an idea for a new feature, please file an issue at\n"
+        footer += f"{url}\n\n"
         self._footer = footer
 
     def add_warning(self, warning: str):
