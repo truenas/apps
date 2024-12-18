f9f884e749ef58545c4f4a8cb3e023dcfcbe4175
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2024-12-18 21:09:10 +0200

    nextcloud: don't make container restart if occ is not available (#1187)

--- ix-dev/stable/nextcloud/templates/macros/nc.jinja.sh
+++ ix-dev/stable/nextcloud/templates/macros/nc.jinja.sh
@@ -21,10 +21,9 @@ run_as "$@"
 
 {% macro hosts_update(values) -%}
 #!/bin/bash
-set -e
 config_file="/var/www/html/config/config.php"
 {# Reason for sed: https://github.com/nextcloud/server/issues/44924 #}
 echo "Updating database and redis host in config.php"
-sed -i "s/\('dbhost' => '\)[^']*postgres:5432',/\1{{ values.consts.postgres_container_name }}:5432',/" "$config_file"
-occ config:system:set redis host --value="{{ values.consts.redis_container_name }}"
+sed -i "s/\('dbhost' => '\)[^']*postgres:5432',/\1{{ values.consts.postgres_container_name }}:5432',/" "$config_file" || { echo "Failed to update database host. Exiting..."; exit 1; }
+occ config:system:set redis host --value="{{ values.consts.redis_container_name }}" || { echo "Failed to update redis host. Continuing..."; exit 0; }
 {%- endmacro -%}
