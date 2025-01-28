c5af138f37c4500361c51655483586c4db694694
Stavros Kois <s.kois@outlook.com>
2025-01-28 18:16:05 +0200

    add some migrations for testing

---
+++ ix-dev/test/nginx/migrations/from_1_0_0_until_1_0_2_to_1_0_3_until_1_0_6.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import yaml
+
+
+def migrate(values):
+    values.update({"some_migrated_field": 1})
+    with open("/tmp/migrations/from_1_0_0_until_1_0_2_to_1_0_3_until_1_0_6.txt", "w") as f:
+        f.write(yaml.dump(values))
+    return values
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        exit(1)
+
+    if os.path.exists(sys.argv[1]):
+        with open(sys.argv[1], "r") as f:
+            print(yaml.dump(migrate(yaml.safe_load(f.read()))))
