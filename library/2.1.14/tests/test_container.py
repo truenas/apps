ffd534927ff1c410eed8ba5d1364556ee2512f9e
Stavros Kois <s.kois@outlook.com>
2025-01-29 17:41:58 +0200

    lib: fix ports handling

--- library/2.1.14/tests/test_container.py
+++ library/2.1.14/tests/test_container.py
@@ -369,6 +369,15 @@ def test_add_ports_with_invalid_host_ips(mock_values):
         c1.add_port({"port_number": 8081, "container_port": 8080, "bind_mode": "published", "host_ips": "invalid"})
 
 
+def test_add_ports_with_empty_host_ips(mock_values):
+    render = Render(mock_values)
+    c1 = render.add_container("test_container", "test_image")
+    c1.healthcheck.disable()
+    c1.add_port({"port_number": 8081, "container_port": 8080, "bind_mode": "published", "host_ips": []})
+    output = render.render()
+    assert output["test_container"]["ports"] == [{"published": 8080, "target": 8081, "protocol": "tcp"}]
+
+
 def test_set_ipc_mode(mock_values):
     render = Render(mock_values)
     c1 = render.add_container("test_container", "test_image")
