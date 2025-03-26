8131d3026d542c0db60b4713fc46251302d1e3f1
Stavros Kois <s.kois@outlook.com>
2025-03-26 18:22:01 +0200

    fix entrypoint and remove accounts from test

--- ix-dev/community/monero-lws/templates/macros/entrypoint.sh
+++ ix-dev/community/monero-lws/templates/macros/entrypoint.sh
@@ -2,8 +2,12 @@
 #!/bin/sh
 
 {% for account in values.lws.accounts %}
+echo "Adding account {{ account.address }}"
 monero-lws-admin add_account "{{ account.address }}" "{{ account.view_key }}"
 {% endfor %}
 
-monero-lws-daemon --db-path={{ values.consts.monero_lws_path }}/light_wallet_server "$@"
+echo "Starting monero-lws-daemon"
+monero-lws-daemon \
+  --db-path "{{ values.consts.monero_lws_path }}/light_wallet_server" \
+  "$@"
 {%- endmacro %}
