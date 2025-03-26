3fa1562eb67fc08809c65d0dc5b0480ed09ce69f
Stavros Kois <s.kois@outlook.com>
2025-03-26 17:54:54 +0200

    polish

--- ix-dev/community/monero-lws/templates/macros/entrypoint.sh
+++ ix-dev/community/monero-lws/templates/macros/entrypoint.sh
@@ -2,8 +2,8 @@
 #!/bin/sh
 
 {% for account in values.lws.accounts %}
-  monero-lws-admin add_account {{ account.address }} {{ account.view_key }}
+monero-lws-admin add_account "{{ account.address }}" "{{ account.view_key }}"
 {% endfor %}
 
-monero-lws-daemon --db-path=/home/monero-lws/.bitmonero/light_wallet_server "$@"
+monero-lws-daemon --db-path={{ values.consts.monero_lws_path }}/light_wallet_server "$@"
 {%- endmacro %}
