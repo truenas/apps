1f8d65d5a369f527c90072ac9991f4282215001d
Martin Joneš <jondytmf@gmail.com>
2025-03-30 12:04:15 +0200

    Trust proxies on local network

--- ix-dev/community/archisteamfarm/macros/entrypoint.sh
+++ ix-dev/community/archisteamfarm/macros/entrypoint.sh
@@ -9,7 +9,12 @@ cat <<EOF > /app/config/IPC.config
 			"HTTP": {
 				"Url": "http://*:1242"
 			}
-		}
+		},
+		"KnownNetworks": [
+			"10.0.0.0/8",
+			"172.16.0.0/12",
+			"192.168.0.0/16"
+		]
 	}
 }
 EOF
