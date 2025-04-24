45fea52020aa1bb7871a31e042a783285e9c3c61
Stavros Kois <s.kois@outlook.com>
2025-04-24 19:04:34 +0300

    use jinja vars instead

--- ix-dev/community/electrs/templates/macros/entrypoint.sh
+++ ix-dev/community/electrs/templates/macros/entrypoint.sh
@@ -1,14 +1,13 @@
 {% macro entrypoint(values) -%}
 #!/bin/sh
 
-ELECTRS_DIR_PATH="{{ values.consts.electrs_dir_path }}"
-CONFIG_FILE_PATH="$ELECTRS_DIR_PATH/config.toml"
+{% set cfg_file_path = "%s/config.toml" | format(values.consts.config_dir_path) %}
 
-mkdir -p $ELECTRS_DIR_PATH
-touch $CONFIG_FILE_PATH
+mkdir -p "{{ values.consts.config_dir_path }}"
+touch "{{ cfg_file_path }}"
 
-sed -i "/auth=/d" $CONFIG_FILE_PATH
-echo 'auth="{{ values.electrs.bitcoind_rpc_user }}:{{ values.electrs.bitcoind_rpc_password }}"' >> $CONFIG_FILE_PATH
+sed -i "/auth=/d" "{{ cfg_file_path }}"
+echo 'auth="{{ values.electrs.bitcoind_rpc_user }}:{{ values.electrs.bitcoind_rpc_password }}"' >> "{{ cfg }}"
 
 electrs "$@"
 {%- endmacro %}
