7952d2b6cb7c39c8e4127568186711725701a504
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2025-04-24 19:58:45 +0300

    Update entrypoint.sh

--- ix-dev/community/electrs/templates/macros/entrypoint.sh
+++ ix-dev/community/electrs/templates/macros/entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p "{{ values.consts.config_dir_path }}"
 touch "{{ cfg_file_path }}"
 
 sed -i "/auth=/d" "{{ cfg_file_path }}"
-echo 'auth="{{ values.electrs.bitcoind_rpc_user }}:{{ values.electrs.bitcoind_rpc_password }}"' >> "{{ cfg }}"
+echo 'auth="{{ values.electrs.bitcoind_rpc_user }}:{{ values.electrs.bitcoind_rpc_password }}"' >> "{{ cfg_file_path }}"
 
 electrs "$@"
 {%- endmacro %}
