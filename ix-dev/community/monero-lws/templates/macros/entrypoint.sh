f5ddaf188ff4b48580fbce77be0a67d23e1c5177
Artur <33733651+Keeqler@users.noreply.github.com>
2025-04-28 12:58:36 -0300

    Monero-LWS: Support account restore height (#2150)
    * feat: add restore height option to monero-lws
    
    * Default restore_height to 0
    
    * quote vars
    
    * by default its not required
    
    ---------
    
    Co-authored-by: Stavros Kois <s.kois@outlook.com>
    Co-authored-by: Stavros Kois <47820033+stavros-k@users.noreply.github.com>

--- ix-dev/community/monero-lws/templates/macros/entrypoint.sh
+++ ix-dev/community/monero-lws/templates/macros/entrypoint.sh
@@ -1,11 +1,34 @@
 {% macro entrypoint(values) -%}
 #!/bin/sh
 
-mkdir -p $HOME/.bitmonero/light_wallet_server
+set -e
+
+LWS_DIR_PATH="{{ values.consts.monero_lws_path }}/light_wallet_server"
+ACCOUNTS_FILE_PATH="{{ values.consts.monero_lws_path }}/.accounts.txt"
+
+mkdir -p "$LWS_DIR_PATH"
+touch "$ACCOUNTS_FILE_PATH"
 
 {% for account in values.lws.accounts %}
-echo "Adding account {{ account.address }}"
-monero-lws-admin add_account "{{ account.address }}" "{{ account.view_key }}"
+address="{{ account.address }}"
+view_key="{{ account.view_key }}"
+restore_height={{ account.restore_height or 0 }}
+account_id="$address:$restore_height"
+
+if grep -q "$address" "$ACCOUNTS_FILE_PATH"; then
+  if ! grep -q "$account_id" "$ACCOUNTS_FILE_PATH"; then
+    echo "Rescanning account $address from block $restore_height"
+    monero-lws-admin rescan "$restore_height" "$address"
+    sed -i "/$address/d" "$ACCOUNTS_FILE_PATH"
+    echo "$account_id" >> "$ACCOUNTS_FILE_PATH"
+  fi
+else
+  echo "Adding account $address"
+  monero-lws-admin add_account "$address" "$view_key"
+  monero-lws-admin rescan "$restore_height" "$address"
+  sed -i '/{{ account.address }}/d' "$ACCOUNTS_FILE_PATH"
+  echo "$account_id" >> "$ACCOUNTS_FILE_PATH"
+fi
 {% endfor %}
 
 echo "Starting monero-lws-daemon"
