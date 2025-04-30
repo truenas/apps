b0586ce1576f51379fae5ad6ba216646f3759673
Stavros Kois <s.kois@outlook.com>
2025-04-30 12:03:40 +0300

    fix

--- ix-dev/community/concource/templates/macros/keys.sh
+++ ix-dev/community/concource/templates/macros/keys.sh
@@ -4,36 +4,40 @@
 set -e
 
 {% set base_cmd = "/usr/local/concourse/bin/concourse generate-key -t" %}
+{% set signing_key = "%s/session_signing_key" | format(values.consts.keys_path) %}
+{% set tsa_host_key = "%s/tsa_host_key" | format(values.consts.keys_path) %}
+{% set worker_key = "%s/worker_key" | format(values.consts.keys_path) %}
+{% set authorized_keys = "%s/authorized_keys.pub" | format(values.consts.keys_path) %}
 
-if [ ! -f "{{ values.consts.keys_path }}/session_signing_key" ]; then
-  echo "Generating session signing key..."; {{ base_cmd }} rsa -f {{ values.consts.keys_path }}/session_signing_key
+if [ ! -f "{{ signing_key }}" ]; then
+  echo "Generating session signing key..."; {{ base_cmd }} rsa -f {{ signing_key }}
 else
   echo "Session signing key already exists."
 fi
 
-if [ ! -f "{{ values.consts.keys_path }}/tsa_host_key" ]; then
-  echo "Generating TSA host key..."; {{ base_cmd }} ssh -f {{ values.consts.keys_path }}/tsa_host_key
+if [ ! -f "{{ tsa_host_key }}" ]; then
+  echo "Generating TSA host key..."; {{ base_cmd }} ssh -f {{ tsa_host_key }}
 else
   echo "TSA host key already exists."
 fi
 
-if [ ! -f "{{ values.consts.keys_path }}/worker_key" ]; then
-  echo "Generating worker key..."; {{ base_cmd }} ssh -f {{ values.consts.keys_path }}/worker_key
+if [ ! -f "{{ worker_key }}" ]; then
+  echo "Generating worker key..."; {{ base_cmd }} ssh -f {{ worker_key }}
 else
   echo "Worker key already exists."
 fi
 
-if [ ! -f "{{ values.consts.keys_path }}/authorized_keys.pub" ]; then
+if [ ! -f "{{ authorized_keys }}" ]; then
   echo "Creating authorized keys file..."
-  touch {{ values.consts.keys_path }}/authorized_keys
+  touch {{ authorized_keys }}
 fi
 
 worker_key=$(cat {{ values.consts.keys_path }}/worker_key.pub)
-if grep -q "$worker_key" {{ values.consts.keys_path }}/authorized_keys.pub; then
+if grep -q "$worker_key" {{ authorized_keys }}; then
   echo "Worker key already exists in authorized keys."
 else
   echo "Adding worker key to authorized keys..."
-  echo "$worker_key" >> {{ values.consts.keys_path }}/authorized_keys.pub
+  echo "$worker_key" >> {{ authorized_keys }}
 fi
 
 echo "Done."
