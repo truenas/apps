5c8a4c26827f3474e471e18ea5b677a46c4dd45a
Stavros Kois <s.kois@outlook.com>
2025-05-07 17:07:55 +0300

    whoops

--- ix-dev/stable/diskoverdata/templates/diskover_macros/fix-nginx.sh
+++ ix-dev/stable/diskoverdata/templates/diskover_macros/fix-nginx.sh
@@ -3,7 +3,7 @@
 # Old installations had the "default" without extension,
 # and after a version the extension was added. Lets cover both cases.
 {% for file in ["/config/nginx/site-confs/default.conf", "/config/nginx/site-confs/default"] %}
-if [ ! -f {{ file }} ]; then
+if [ -f {{ file }} ]; then
   if grep -q "root /app/www/public" {{ file }}; then
       echo "Fixing nginx [{{ file }}] file"
       sed -i 's/root \/app\/www\/public/root \/app\/diskover-web\/public/g' {{ file }} || { echo "Failed to fix nginx [{{ file }}] file"; exit 1; }
