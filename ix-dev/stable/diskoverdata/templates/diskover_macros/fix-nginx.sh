22ae00f7157472c594f65c863f7bc23f435465de
Stavros Kois <s.kois@outlook.com>
2025-05-07 17:06:25 +0300

    diskoverdata: fix indexing additional storage

---
+++ ix-dev/stable/diskoverdata/templates/diskover_macros/fix-nginx.sh
@@ -0,0 +1,13 @@
+{% macro fix_nginx() -%}
+#!/bin/bash
+# Old installations had the "default" without extension,
+# and after a version the extension was added. Lets cover both cases.
+{% for file in ["/config/nginx/site-confs/default.conf", "/config/nginx/site-confs/default"] %}
+if [ ! -f {{ file }} ]; then
+  if grep -q "root /app/www/public" {{ file }}; then
+      echo "Fixing nginx [{{ file }}] file"
+      sed -i 's/root \/app\/www\/public/root \/app\/diskover-web\/public/g' {{ file }} || { echo "Failed to fix nginx [{{ file }}] file"; exit 1; }
+  fi
+fi
+{% endfor %}
+{%- endmacro %}
--- ix-dev/stable/diskoverdata/templates/diskover_macros/setup-cron.sh
+++ ix-dev/stable/diskoverdata/templates/diskover_macros/setup-cron.sh
@@ -1,14 +1,5 @@
 {% macro setup_cron(values) -%}
 #!/bin/bash
-# Old installations had the "default" without extension,
-# and after a version the extension was added. Lets cover both cases.
-{% for file in ["/config/nginx/site-confs/default.conf", "/config/nginx/site-confs/default"] %}
-if grep -q "root /app/www/public" {{ file }}; then
-    echo "Fixing nginx [{{ file }}] file"
-    sed -i 's/root \/app\/www\/public/root \/app\/diskover-web\/public/g' {{ file }} || { echo "Failed to fix nginx [{{ file }}] file"; exit 1; }
-fi
-{% endfor %}
-
 {{ check_path("/data") }}
 [ ! "$(ls -A /data)" ] && {{ create_dummy_file("/data") }}
 
@@ -28,11 +19,13 @@ echo "Empty directory found, writing a dummy file at [{{ path }}] to trigger ind
 {%- endmacro %}
 
 {% macro check_path(path) -%}
-if grep -q "/app/diskover/diskover.py {{ path }}" /config/crontab; then
-    echo "------------------------------------WARNING-----------------------------------"
-    echo "A crontab entry for [{{ path }}] has been found in /config/crontab".
-    echo "This is no longer needed as it is now handled in the /etc/crontabs/abc file."
-    echo "Please remove the entry from /config/crontab"
-    echo "------------------------------------------------------------------------------"
+if [ -f /config/crontab ]; then
+    if grep -q "/app/diskover/diskover.py {{ path }}" /config/crontab; then
+        echo "------------------------------------WARNING-----------------------------------"
+        echo "A crontab entry for [{{ path }}] has been found in /config/crontab".
+        echo "This is no longer needed as it is now handled in the /etc/crontabs/abc file."
+        echo "Please remove the entry from /config/crontab"
+        echo "------------------------------------------------------------------------------"
+    fi
 fi
 {%- endmacro %}
