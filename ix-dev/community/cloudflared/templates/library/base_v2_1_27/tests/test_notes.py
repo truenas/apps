d59ff588198709a07d0876846b1cba960913c23a
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2025-05-14 15:08:51 +0300

    cloudflared: fix dns-proxy and tunnel (#2269)
    * cloudflared: polish
    
    * remove old lib
    
    * adapt command
    
    * fixes
    
    * fixes
    
    * unneeded
    
    * bump
    
    * fix
    
    * use flags
    
    * resever
    
    * order
    
    * ci
    
    * fix
    
    * version
    
    * hash
    
    * lib

--- ix-dev/community/cloudflared/templates/library/base_v2_1_27/tests/test_notes.py
+++ ix-dev/community/cloudflared/templates/library/base_v2_1_27/tests/test_notes.py
@@ -32,6 +32,13 @@ def test_notes(mock_values):
         output["x-notes"]
         == """# Test App
 
+## Security
+
+### Container: [test_container]
+
+- Is running as unknown user
+- Is running as unknown group
+
 ## Bug Reports and Feature Requests
 
 If you find a bug in this app or have an idea for a new feature, please file an issue at
@@ -45,6 +52,7 @@ def test_notes_on_non_enterprise_train(mock_values):
     mock_values["ix_context"]["app_metadata"]["train"] = "community"
     render = Render(mock_values)
     c1 = render.add_container("test_container", "test_image")
+    c1.set_user(568, 568)
     c1.healthcheck.disable()
     output = render.render()
     assert (
@@ -65,6 +73,7 @@ def test_notes_with_warnings(mock_values):
     render.notes.add_warning("this is not properly configured. fix it now!")
     render.notes.add_warning("that is not properly configured. fix it later!")
     c1 = render.add_container("test_container", "test_image")
+    c1.set_user(568, 568)
     c1.healthcheck.disable()
     output = render.render()
     assert (
@@ -90,6 +99,7 @@ def test_notes_with_deprecations(mock_values):
     render.notes.add_deprecation("this is will be removed later. fix it now!")
     render.notes.add_deprecation("that is will be removed later. fix it later!")
     c1 = render.add_container("test_container", "test_image")
+    c1.set_user(568, 568)
     c1.healthcheck.disable()
     output = render.render()
     assert (
@@ -120,6 +130,7 @@ some other info.
 """
     )
     c1 = render.add_container("test_container", "test_image")
+    c1.set_user(568, 568)
     c1.healthcheck.disable()
     output = render.render()
     assert (
@@ -155,6 +166,13 @@ some other info.
     )
     c1 = render.add_container("test_container", "test_image")
     c1.healthcheck.disable()
+    c1.set_privileged(True)
+    c1.set_user(0, 0)
+    c1.set_ipc_mode("host")
+    c1.set_cgroup("host")
+    c1.set_tty(True)
+    c1.remove_security_opt("no-new-privileges")
+    c1.restart.set_policy("on-failure", 1)
     output = render.render()
     assert (
         output["x-notes"]
@@ -162,6 +180,7 @@ some other info.
 
 ## Warnings
 
+- Container [test_container] is running with a TTY, Logs will not appear correctly in the UI due to an [upstream bug](https://github.com/docker/docker-py/issues/1394)
 - this is not properly configured. fix it now!
 - that is not properly configured. fix it later!
 
@@ -169,6 +188,19 @@ some other info.
 
 - this is will be removed later. fix it now!
 - that is will be removed later. fix it later!
+
+## Security
+
+### Container: [test_container]
+
+**This container is short-lived.**
+
+- Is running with privileged mode enabled
+- Is running as root user
+- Is running as root group
+- Is running with host IPC namespace
+- Is running with host cgroup namespace
+- Is running without [no-new-privileges] security option
 
 ## Additional info
 
@@ -180,5 +212,5 @@ some other info.
 If you find a bug in this app or have an idea for a new feature, please file an issue at
 https://ixsystems.atlassian.net
 
-"""
+"""  # noqa
     )
