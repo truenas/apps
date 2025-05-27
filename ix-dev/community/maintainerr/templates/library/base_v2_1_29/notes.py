baaed5239cc6d2232a88e01dd2eae126febc3036
Stavros Kois <s.kois@outlook.com>
2025-05-27 14:41:45 +0300

    update lib

--- ix-dev/community/maintainerr/templates/library/base_v2_1_29/notes.py
+++ ix-dev/community/maintainerr/templates/library/base_v2_1_29/notes.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from render import Render
 
+SHORT_LIVED = "short-lived"
+
 
 class Notes:
     def __init__(self, render_instance: "Render"):
@@ -11,6 +13,7 @@ class Notes:
         self._app_train: str = ""
         self._warnings: list[str] = []
         self._deprecations: list[str] = []
+        self._security: dict[str, list[str]] = {}
         self._header: str = ""
         self._body: str = ""
         self._footer: str = ""
@@ -47,13 +50,50 @@ class Notes:
     def add_warning(self, warning: str):
         self._warnings.append(warning)
 
+    def _prepend_warning(self, warning: str):
+        self._warnings.insert(0, warning)
+
     def add_deprecation(self, deprecation: str):
         self._deprecations.append(deprecation)
 
     def set_body(self, body: str):
         self._body = body
 
+    def scan_containers(self):
+        for name, c in self._render_instance._containers.items():
+            if self._security.get(name) is None:
+                self._security[name] = []
+
+            if c.restart._policy == "on-failure":
+                self._security[name].append(SHORT_LIVED)
+
+            if c._privileged:
+                self._security[name].append("Is running with privileged mode enabled")
+
+            run_as = c._user.split(":") if c._user else [-1, -1]
+            if run_as[0] in ["0", -1]:
+                self._security[name].append(f"Is running as {'root' if run_as[0] == '0' else 'unknown'} user")
+            if run_as[1] in ["0", -1]:
+                self._security[name].append(f"Is running as {'root' if run_as[1] == '0' else 'unknown'} group")
+
+            if c._ipc_mode == "host":
+                self._security[name].append("Is running with host IPC namespace")
+            if c._cgroup == "host":
+                self._security[name].append("Is running with host cgroup namespace")
+            if "no-new-privileges=true" not in c._security_opt.render():
+                self._security[name].append("Is running without [no-new-privileges] security option")
+            if c._tty:
+                self._prepend_warning(
+                    f"Container [{name}] is running with a TTY, "
+                    "Logs will not appear correctly in the UI due to an [upstream bug]"
+                    "(https://github.com/docker/docker-py/issues/1394)"
+                )
+
+        self._security = {k: v for k, v in self._security.items() if v}
+
     def render(self):
+        self.scan_containers()
+
         result = self._header
 
         if self._warnings:
@@ -67,6 +107,19 @@ class Notes:
             for deprecation in self._deprecations:
                 result += f"- {deprecation}\n"
             result += "\n"
+
+        if self._security:
+            result += "## Security\n\n"
+            for c_name, security in self._security.items():
+                if SHORT_LIVED in security and len(security) == 0:
+                    continue
+                result += f"### Container: [{c_name}]"
+                if SHORT_LIVED in security:
+                    result += "\n\n**This container is short-lived.**"
+                result += "\n\n"
+                for s in [s for s in security if s != "short-lived"]:
+                    result += f"- {s}\n"
+                result += "\n"
 
         if self._body:
             result += self._body.strip() + "\n\n"
