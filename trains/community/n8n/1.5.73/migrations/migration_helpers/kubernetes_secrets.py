1281ae0153b2a30c2ffae3fd8e6a59aa2d64a6a4
Stavros Kois <s.kois@outlook.com>
2025-05-30 17:14:45 +0300

    untouch

--- trains/community/n8n/1.5.73/migrations/migration_helpers/kubernetes_secrets.py
+++ trains/community/n8n/1.5.73/migrations/migration_helpers/kubernetes_secrets.py
@@ -8,7 +8,9 @@ def get_value_from_secret(secrets=None, secret_name=None, key=None):
     for curr_secret_name, curr_data in secrets.items():
         if curr_secret_name.endswith(secret_name):
             if not curr_data.get(key, None):
-                raise ValueError(f"Expected [{key}] to be set in secret [{curr_secret_name}]")
+                raise ValueError(
+                    f"Expected [{key}] to be set in secret [{curr_secret_name}]"
+                )
             return curr_data[key]
 
     raise ValueError(f"Secret [{secret_name}] not found")
