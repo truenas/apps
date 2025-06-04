96148f8a616b7e733f3826e64b6b25cbfdca6fef
Stavros Kois <s.kois@outlook.com>
2025-06-04 17:08:57 +0300

    add web container

--- ix-dev/community/garage/templates/macros/setup.sh
+++ ix-dev/community/garage/templates/macros/setup.sh
@@ -10,10 +10,14 @@ fi
 echo "Updating configuration file at {{ cfg_file }}"
 
 {%- set base_cmd = "dasel put --file %s --type" |format(cfg_file) %}
+{% for item in values.garage.additional_options %}
+{{ base_cmd }} {{ item.type }} "{{ item.path }}" --value "{{ item.value }}"
+{% endfor %}
+
+{{ base_cmd }} int ".replication_factor" --value 1
 {{ base_cmd }} string ".metadata_dir" --value "{{ values.consts.metadata_path }}"
 {{ base_cmd }} string ".data_dir" --value "{{ values.consts.data_path }}"
 {{ base_cmd }} string ".metadata_snapshots_dir" --value "{{ values.consts.metadata_snapshots_path }}"
-{{ base_cmd }} int ".replication_factor" --value 1
 
 {{ base_cmd }} string ".rpc_bind_addr" --value "0.0.0.0:{{ values.network.rpc_port.port_number }}"
 
@@ -21,7 +25,7 @@ echo "Updating configuration file at {{ cfg_file }}"
 {{ base_cmd }} string ".s3_api.s3_region" --value "{{ values.garage.region }}"
 
 {{ base_cmd }} string ".s3_web.bind_addr" --value "0.0.0.0:{{ values.network.s3_web_port.port_number }}"
-{{ base_cmd }} string ".s3_web.root_domain" --value ".web.garage"
+{{ base_cmd }} string ".s3_web.root_domain" --value "{{ values.garage.s3_web_root_domain }}"
 
 {{ base_cmd }} string ".admin.api_bind_addr" --value "0.0.0.0:{{ values.network.admin_port.port_number }}"
 
