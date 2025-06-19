f65a37530bc700722aa5b60245ce7690b0705223
Stavros Kois <s.kois@outlook.com>
2025-06-19 18:56:00 +0300

    fixes

--- ix-dev/community/filebrowser/templates/macros/script.sh
+++ ix-dev/community/filebrowser/templates/macros/script.sh
@@ -1,11 +1,15 @@
 {% macro script(values) -%}
 #!/bin/sh
-{% set mappings = {
+{%- set mappings = {
   "%s/filebrowser.json"|format(values.consts.config_path): "%s/settings.json"|format(values.consts.config_path),
-  "%s/database.db"|format(values.consts.config_path): "%s/filebrowser.db"|format(values.consts.config_path)
+  "%s/database.db"|format(values.consts.config_path): "%s/filebrowser.db"|format(values.consts.config_path),
 } %}
 
-{% for src, dest in mappings.items() %}
-[ -f {{ src }} ] && {echo "File found at [{{ src }}], renaming to {{ dest }}"; mv {{ src }} {{ dest }}; }
-{% endfor %}
+echo "Migrating FileBrowser configuration files..."
+
+{%- for src, dest in mappings.items() %}
+echo "Checking for file at [{{ src }}]..."
+[ -f {{ src }} ] && { echo "File found at [{{ src }}], renaming to {{ dest }}"; mv "{{ src }}" "{{ dest }}"; }
+{%- endfor %}
+echo "Migration complete."
 {%- endmacro %}
