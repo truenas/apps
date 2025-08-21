a9c3fa3c5a17e140cefcb5526155ca14ba252295
Stavros Kois <s.kois@outlook.com>
2025-08-21 18:27:24 +0300

    test

--- ix-dev/community/tubearchivist/templates/library/base_v2_1_47/deps_elastic.py
+++ ix-dev/community/tubearchivist/templates/library/base_v2_1_47/deps_elastic.py
@@ -52,7 +52,7 @@ class ElasticSearchContainer:
         c.environment.add_env("ELASTIC_PASSWORD", config["password"])
         c.environment.add_env("http.port", self.get_port())
         c.environment.add_env("path.data", self._data_dir)
-        c.environment.add_env("path.repo", f"{self._data_dir}/snapshot")
+        c.environment.add_env("path.repo", self.get_snapshots_dir())
         c.environment.add_env("node.name", config["node_name"])
         c.environment.add_env("discovery.type", "single-node")
         c.environment.add_env("xpack.security.enabled", True)
@@ -90,3 +90,6 @@ class ElasticSearchContainer:
 
     def get_url(self):
         return f"http://{self._name}:{self.get_port()}"
+
+    def get_snapshots_dir(self):
+        return f"{self._data_dir}/snapshots"
