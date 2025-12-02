7851dcb71b79aced26aaf9aac0edb99089b5e6dd
Stavros Kois <47820033+stavros-k@users.noreply.github.com>
2025-12-02 11:44:15 +0200

    immich: remove unused pgverctors from db image (#3751)
    * immich: remove unused pgverctors from db image
    
    * fix renovate
    
    * comment
    
    * update extension as well

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -267,8 +267,8 @@ module.exports = {
       ["wger/server"]
     ),
     customVersioning(
-      // 15-vectorchord0.3.0-pgvectors0.2.0
-      "^15-vectorchord(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)-pgvectors0.2.0$",
+      // 15-vectorchord0.3.0
+      "^15-vectorchord(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)$",
       ["ghcr.io/immich-app/postgres"]
     ),
     customVersioning(
