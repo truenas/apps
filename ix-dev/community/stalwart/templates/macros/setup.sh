eb862eea4fdf5f40ee67f46b1265503f20de85a4
Stavros Kois <s.kois@outlook.com>
2026-01-21 19:57:58 +0200

    add config

---
+++ ix-dev/community/stalwart/templates/macros/setup.sh
@@ -0,0 +1,20 @@
+{% macro setup(values, cfg) -%}
+#!/bin/bash
+{%- set cfg_file = values.consts.config_file_path %}
+if [ ! -f {{ cfg_file }} ]; then
+    echo "Creating a new configuration file at {{ cfg_file }}"
+    touch {{ cfg_file }}
+fi
+
+{%- set base_cmd = "dasel put --file %s --type" |format(cfg_file) %}
+{% set keys = cfg | map(attribute="path") | list %}
+
+echo "Updating configuration file at {{ cfg_file }}"
+
+{% for item in cfg %}
+echo "Setting option [{{ item.path }}] to [{{ item.value }}]"
+{{ base_cmd }} {{ item.type }} "{{ item.path }}" --value "{{ item.value }}"
+{% endfor %}
+
+echo "Configuration file updated successfully at {{ cfg_file }}"
+{% endmacro %}
