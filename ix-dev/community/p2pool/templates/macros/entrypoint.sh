2dfb1d89a08267b60b12c075857bb11287db0d5e
Stavros Kois <s.kois@outlook.com>
2026-02-11 18:18:19 +0200

    reduce compose size

--- ix-dev/community/p2pool/templates/macros/entrypoint.sh
+++ ix-dev/community/p2pool/templates/macros/entrypoint.sh
@@ -3,14 +3,15 @@
 
 set -e
 
-# This avoids resolving host.docker.internal to ipv6.
-# Monerod does not bind the zmq pub port to ipv6 for example, so we'd be unable to connect.
+{%- if values.p2pool.use_remote_node %}
+p2pool --host {{ values.p2pool.remote_node_host }} "$@"
+{%- else %}
+{#
+  This avoids resolving host.docker.internal to ipv6.
+  Monerod does not bind the zmq pub port to ipv6 for example, so we'd be unable to connect.
+#}
 HOST_IP=$(getent ahostsv4 host.docker.internal | awk '{print $1}' | head -n 1)
-
-{% if values.p2pool.use_remote_node %}
-  p2pool --host {{ values.p2pool.remote_node_host }} "$@"
-{% else %}
-  p2pool --host $HOST_IP "$@"
-{% endif %}
+p2pool --host $HOST_IP "$@"
+{%- endif %}
 
 {%- endmacro %}
