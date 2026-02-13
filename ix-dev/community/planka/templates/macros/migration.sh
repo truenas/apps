d3eba2318bc4c36bfc6aac1b9e764fdb0e14cc70
Stavros Kois <s.kois@outlook.com>
2026-02-13 19:19:44 +0200

    order

--- ix-dev/community/planka/templates/macros/migration.sh
+++ ix-dev/community/planka/templates/macros/migration.sh
@@ -1,16 +1,16 @@
 {%- macro migration(values) %}
 set -e
 
-function mark_completed() {
-  echo "Marking migration as completed by creating /app/data/.migration_completed"
-  echo "DO NOT REMOVE THIS FILE\n" > /app/data/.migration_completed
-  echo "Keep this file to prevent the migration from running again.\n" >> /app/data/.migration_completed
-  echo "It is only safe to remove this file once migration handling code is removed" >> /app/data/.migration_completed
-  echo "from the TrueNAS app and you have upgraded to a newer version of the TrueNAS app." >> /app/data/.migration_completed
-}
-
 function echo() {
   /bin/echo "[migration.sh] $@"
+}
+
+function mark_completed() {
+  echo "Marking migration as completed by creating /app/data/.migration_completed"
+  echo -e "DO NOT REMOVE THIS FILE\n" > /app/data/.migration_completed
+  echo -e "Keep this file to prevent the migration from running again.\n" >> /app/data/.migration_completed
+  echo -e "It is only safe to remove this file once migration handling code is removed" >> /app/data/.migration_completed
+  echo -e "from the TrueNAS app and you have upgraded to a newer version of the TrueNAS app." >> /app/data/.migration_completed
 }
 
 {% set is_install = values.get("ix_context", {}).get("is_install", true) %}
