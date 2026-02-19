6860d9ce6a0a70f6b027aef4a5ccafe58cd2b94c
Stavros Kois <s.kois@outlook.com>
2026-02-19 18:42:12 +0200

    do as much calcs in the tempalte as possible and only allow 'single' mode.
    Config file can still be edited by the user, just the options set by they UI, has to still be set by the UI

--- ix-dev/community/xmrig/templates/macros/entrypoint.sh
+++ ix-dev/community/xmrig/templates/macros/entrypoint.sh
@@ -1,10 +1,9 @@
-{% macro entrypoint(values) -%}
+{%- macro entrypoint(values) -%}
 #!/bin/sh
 
 set -e
 
 EXAMPLE_CONFIG_FILE_PATH="$HOME/xmrig-config.json"
-QUESTIONS_CONFIG_FILE_PATH="$HOME/xmrig-questions.json"
 USER_CONFIG_FILE_PATH="$HOME/.config/xmrig.json"
 
 if [ ! -e "$USER_CONFIG_FILE_PATH" ]; then
@@ -12,57 +11,52 @@ if [ ! -e "$USER_CONFIG_FILE_PATH" ]; then
   cp $EXAMPLE_CONFIG_FILE_PATH $USER_CONFIG_FILE_PATH
 fi
 
-if [ ! -e "$QUESTIONS_CONFIG_FILE_PATH" ]; then
-  echo '{}' > "$QUESTIONS_CONFIG_FILE_PATH"
-fi
-
 jq_write() {
-  jq "$1" "$QUESTIONS_CONFIG_FILE_PATH" > "$QUESTIONS_CONFIG_FILE_PATH.tmp" && mv "$QUESTIONS_CONFIG_FILE_PATH.tmp" "$QUESTIONS_CONFIG_FILE_PATH"
+  jq "$1" "$USER_CONFIG_FILE_PATH" > "$USER_CONFIG_FILE_PATH.tmp" && mv "$USER_CONFIG_FILE_PATH.tmp" "$USER_CONFIG_FILE_PATH"
 }
 
-if [ "{{ values.xmrig.use_custom_config_file }}" = "False" ]; then
-  if [ "{{ values.xmrig.remote_pool_address }}" != '' ]; then
-    jq_write '.pools[0].url = "{{ values.xmrig.remote_pool_address }}"'
-  else
-    jq_write '.pools[0].url = "host.docker.internal:{{ values.xmrig.local_xmrig_port }}"'
-  fi
+{%- set pool_addr =
+  values.xmrig.remote_pool_address
+  if values.xmrig.remote_pool_address
+  else "host.docker.internal:%s" | format(values.xmrig.local_xmrig_port)
+%}
 
-  if [ "{{ values.xmrig.pool_username }}" != '' ]; then
-    jq_write '.pools[0].user = "{{ values.xmrig.pool_username }}"'
-  fi
+jq_write '.pools[0].url = "{{ pool_addr }}"'
 
-  if [ "{{ values.xmrig.pool_password }}" != '' ]; then
-    jq_write '.pools[0].pass = "{{ values.xmrig.pool_password }}"'
-  fi
+{%- if values.xmrig.pool_username %}
+jq_write '.pools[0].user = "{{ values.xmrig.pool_username }}"'
+{%- endif %}
 
-  if [ "{{ values.xmrig.algorithm }}" != '' ]; then
-    jq_write '.pools[0].algo = "{{ values.xmrig.algorithm }}"'
-  fi
+{%- if values.xmrig.pool_password %}
+jq_write '.pools[0].pass = "{{ values.xmrig.pool_password }}"'
+{%- endif %}
 
-  if [ "{{ values.xmrig.coin }}" != '' ]; then
-    jq_write '.pools[0].coin = "{{ values.xmrig.coin }}"'
-  fi
+{%- if values.xmrig.algorithm %}
+jq_write '.pools[0].algo = "{{ values.xmrig.algorithm }}"'
+{%- endif %}
 
-  jq_write '.cpu.enabled = true'
-  jq_write '.cpu.priority = {{ values.xmrig.cpu_priority }}'
-  jq_write '.cpu["*"].threads = {{ values.xmrig.cpu_threads }}'
-  jq_write '.randomx["1gb-pages"] = {{ values.xmrig.use_1gb_huge_pages | lower }}'
-  jq_write '.randomx.rdmsr = {{ values.xmrig.use_msr_mod | lower }}'
+{%- if values.xmrig.coin %}
+jq_write '.pools[0].coin = "{{ values.xmrig.coin }}"'
+{%- endif %}
 
-  if [ "{{ values.xmrig.use_proxy }}" = "True" ]; then
-    if [ "{{ values.xmrig.use_arti_app_proxy }}" = "True" ]; then
-      jq_write '.pools[0].socks5 = "host.docker.internal:{{ values.xmrig.arti_app_proxy_port }}"'
-    else
-      jq_write '.pools[0].socks5 = "{{ values.xmrig.proxy_host }}:{{ values.xmrig.proxy_port }}"'
-    fi
-  fi
+jq_write '.cpu.enabled = true'
+jq_write '.cpu.priority = {{ values.xmrig.cpu_priority }}'
+jq_write '.cpu["*"].threads = {{ values.xmrig.cpu_threads }}'
+jq_write '.randomx["1gb-pages"] = {{ values.xmrig.use_1gb_huge_pages | lower }}'
+jq_write '.randomx.rdmsr = {{ values.xmrig.use_msr_mod | lower }}'
 
-  jq_write '.["donate-level"] = {{ values.xmrig.donate_percentage }}'
+{%- if values.xmrig.use_proxy %}
+  {%- set proxy_addr =
+    "host.docker.internal:%s" | format(values.xmrig.arti_app_proxy_port)
+    if values.xmrig.use_arti_app_proxy
+    else "%s:%s" | format(values.xmrig.proxy_host, values.xmrig.proxy_port)
+  %}
+  jq_write '.pools[0].socks5 = "{{ proxy_addr }}"'
+{%- endif %}
 
-  cat $QUESTIONS_CONFIG_FILE_PATH
-  xmrig -c $QUESTIONS_CONFIG_FILE_PATH "$@"
-else
-  xmrig "$@"
-fi
+jq_write '.["donate-level"] = {{ values.xmrig.donate_percentage }}'
+
+cat $USER_CONFIG_FILE_PATH
+xmrig -c $USER_CONFIG_FILE_PATH "$@"
 
 {%- endmacro %}
