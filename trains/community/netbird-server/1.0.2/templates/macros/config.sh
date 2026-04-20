f27339831da0f262ee088f5bf7066cfb89da30e9
sonicaj <waqarsonic1@gmail.com>
2026-04-20 12:01:00 +0000

    Publish new changes in catalog [skip ci]

---
+++ trains/community/netbird-server/1.0.2/templates/macros/config.sh
@@ -0,0 +1,22 @@
+{% macro config(values, cfg=[]) -%}
+#!/bin/sh
+{%- set cfg_path = "%s/%s" | format(values.consts.config_path, values.consts.config_file_name) %}
+if [ ! -f "{{ cfg_path }}" ]; then
+  echo "File [{{ cfg_path }}] does not exist. Creating a new config file..."
+  touch "{{ cfg_path }}"
+else
+  echo "File [{{ cfg_path }}] exists!"
+fi
+
+echo "Updating [{{ cfg_path }}] file..."
+{%- for c in cfg %}
+{%- for key, value in c.items() %}
+echo ''
+echo "Updating [{{ key }}] key..."
+yq -i '.{{ key }} = {{ value|tojson }}' "{{ cfg_path }}"
+echo "New value for [{{ key }}]: $(yq '.{{ key }}' "{{ cfg_path }}")"
+{%- endfor %}
+{%- endfor %}
+
+echo "Done!"
+{% endmacro %}
