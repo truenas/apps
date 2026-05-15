652edae9bd1fcac4825c56671828e381792cdb83
Stavros Kois <s.kois@outlook.com>
2026-05-15 19:13:20 +0300

    add glance

---
+++ ix-dev/community/glance/README.md
@@ -0,0 +1,8 @@
+# Invidious
+
+[Invidious](https://invidious.io/) is an alternative front-end to YouTube.
+
+Additional configuration can be specified
+
+- Via [environment variables](https://github.com/iv-org/invidious/pull/1702)
+- By editing the file `/config/config.yaml` (see [example](https://github.com/iv-org/invidious/blob/master/config/config.example.yml))
---
+++ ix-dev/community/glance/templates/macros/config.sh
@@ -0,0 +1,30 @@
+{% macro config(values, cfg=[]) -%}
+#!/bin/sh
+{%- set cfg_path = "%s/glance.yml"|format(values.consts.config_path) %}
+if [ ! -f "{{ cfg_path }}" ]; then
+  echo "File [{{ cfg_path }}] does not exist. Creating a default config..."
+  touch "{{ cfg_path }}"
+else
+  echo "File [{{ cfg_path }}] exists!"
+fi
+
+echo "Updating [{{ cfg_path }}] file..."
+{%- for c in cfg %}
+{%- for key, value in c.items() %}
+echo ''
+echo "Updating [{{ key }}] key..."
+yq -i '.{{ key }} = {{ value|tojson }}' "{{ cfg_path }}"
+echo "New value for [{{ key }}]: $(yq '.{{ key }}' "{{ cfg_path }}")"
+{%- endfor %}
+{%- endfor %}
+
+result=$(yq '. | has("pages")' "{{ cfg_path }}")
+if [ "$result" != "true" ]; then
+  echo "No pages key found. Adding an empty one..."
+  yq -i '.pages = []' "{{ cfg_path }}"
+else
+  echo "Pages key already exists."
+fi
+
+echo "Done!"
+{% endmacro %}
